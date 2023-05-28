4f0555ebef018b290f493ca5f7bede475ff5abc9
Lekha <Lekha.kumaradhas@gmail.com>
2023-05-28 11:42:07 +1000

    Updated main.js and backend

--- backend/api/GetResumeCounter/index.js
+++ backend/api/GetResumeCounter/index.js
@@ -1,13 +1,28 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+module.exports = async function (context, req, counter) {
+    if (!counter) {
+        context.log("Counter not found");
+    }
+    else {
+        if (req.method == 'POST') {
+            counter.count = counter.count + 1;
+            context.log("Post method triggered");
+            context.bindings.counterupd = JSON.stringify({
+                id: "1",
+                count: counter.count
+            });
+            context.res = {
+                status: 200,
+                body: "Counter updated"
+            }
+        } else {
+            context.res = {
+                status: 200,
+                body: {
+                    "count": counter.count
+                }
+            }
+        }
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    }
+};
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
--- frontend/main.js
+++ frontend/main.js
@@ -1,20 +1,27 @@
-window.addEventListener('DOMContentLoaded', (event) =>{
+window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApiUrl = 'https://getresumecounter.azurewebsites.net/api/GetResumeCounter?code=Q/LkPt0mhQKdP8DoE3DdllAYdFQO//58Iq7AoS6JUBj9FgJ86Rqt8A==';
+const functionApiUrl = 'https://azure-resume-app.azurewebsites.net/api/GetResumeCounter?code=K1ocRh28muc6QYLj6fj57M6_7MsywKSMBmw_6yuNZVbnAzFudvYFmw==';
 const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApiUrl).then(response => {
-        return response.json()
-    }).then(response =>{
-        console.log("Website called function API.");
-        count =  response.count;
-        document.getElementById("counter").innerText = count;
-    }).catch(function(error){
-        console.log(error);
-    });
-    return count;
+    fetch(localFunctionApi, {
+        method: "POST"
+    })
+        .then(response => {
+            fetch(functionApiUrl).then(response => {
+                return response.json()
+            }).then(response => {
+                console.log("Website called function API.");
+                count = response.count;
+                document.getElementById("counter").innerText = count;
+            }).catch(function (error) {
+                console.log(error);
+            });
+            return count;
+
+        });
+
 }
